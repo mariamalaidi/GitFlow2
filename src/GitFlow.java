13bb47fccd9f2eba5b723c7c1f97397c659afffc
mariamalaidi <mariam.alaide@gmail.com>
2021-01-07 20:06:19 +0200

    Development

--- src/GitFlow.java
+++ src/GitFlow.java
@@ -1,4 +1,7 @@
 public class GitFlow {
+    public void add_Feature1(){
+        System.out.println("This is feature1");
+    }
     public static void main(String[] args) {
         System.out.println("This is gitFlow");
     }

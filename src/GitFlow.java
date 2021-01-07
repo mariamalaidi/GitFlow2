fd1f1b5edeab8231fb9f63c5e26b56e5c75ea56a
mariamalaidi <mariam.alaide@gmail.com>
2021-01-07 20:11:16 +0200

    updating feature 1

--- src/GitFlow.java
+++ src/GitFlow.java
@@ -1,6 +1,7 @@
 public class GitFlow {
     public void add_Feature1(){
         System.out.println("This is feature1");
+        System.out.println("Updating Feature1 ");
     }
     public static void main(String[] args) {
         System.out.println("This is gitFlow");
